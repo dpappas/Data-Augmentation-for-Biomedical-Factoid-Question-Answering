2202f5050e98f2bea3ee342b1787140e0d34fee9
dvpappas <dvpappas89@gmail.com>
2022-10-24 13:07:41 +0300

    Added info about data

--- question_generation/t5_question_generation.py
+++ question_generation/t5_question_generation.py
@@ -74,32 +74,5 @@ for row_num in range(len(all_questions)):
     for col_num in range(len(all_questions[row_num])):
         row.write(col_num, all_questions[row_num][col_num])
 
-# book.save("questions.xls")
 book.save(sys.argv[1])
-
-
-
-'''
-
-cd t5_multitask_api/
-source venv/bin/activate
-
-python3.6 t5_question_generation.py questions_2.xls "02-2019"  "03-2019" 40000
-python3.6 t5_question_generation.py questions_3.xls "03-2019"  "04-2019" 40000
-python3.6 t5_question_generation.py questions_4.xls "04-2019"  "05-2019" 40000
-python3.6 t5_question_generation.py questions_5.xls "05-2019"  "06-2019" 40000
-python3.6 t5_question_generation.py questions_6.xls "06-2019"  "07-2019" 40000
-python3.6 t5_question_generation.py questions_6.xls "01-2019"  "02-2019" 40000
-
-
-abstract = "LQTS is typically inherited in an autosomal dominant manner. An exception is LQTS associated with sensorineural deafness (known as Jervell and Lange-Nielsen syndrome), which is inherited in an autosomal recessive manner. Most individuals diagnosed with LQTS have an affected parent. The proportion of LQTS caused by a de novo pathogenic variant is small. Each child of an individual with autosomal dominant LQTS has a 50% risk of inheriting the pathogenic variant. Penetrance of the disorder may vary. Prenatal testing for pregnancies at increased risk and preimplantation genetic diagnosis are possible once the pathogenic variant(s) have been identified in the family.".strip()
-
-snippets = [
-"Coronavirus disease 2019 is a respiratory infection caused by severe acute respiratory syndrome coronavirus 2 originating in Wuhan China in 2019.",
-]
-
-abstract = "Within the last five years, the State of Texas has experienced either transmission or outbreaks of Ebola, chikungunya, West Nile, and Zika virus infections. Autochthonous transmission of neglected parasitic and bacterial diseases has also become increasingly reported. The rise of such emerging and neglected tropical diseases (NTDs) has not occurred by accident but instead reflects rapidly evolving changes and shifts in a "new" Texas beset by modern and globalizing forces that include rapid expansions in population together with urbanization and human migrations, altered transportation patterns, climate change, steeply declining vaccination rates, and a new paradigm of poverty known as "blue marble health." Summarized here are the major NTDs now affecting Texas. In addition to the vector-borne viral diseases highlighted above, there also is a high level of parasitic infections, including Chagas disease, trichomoniasis, and possibly leishmaniasis and toxocariasis, as well as typhus-group rickettsiosis, a vector-borne bacterial infection. I also highlight some of the key shifts in emerging and neglected disease patterns, partly due to an altered and evolving economic and ecological landscape in the new Texas, and provide some preliminary disease burden estimates for the major prevalent and incident NTDs.".strip()
-'''
-
-
 
